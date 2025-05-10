661ae9561ed094e927616a05a77c98e3cc658452
GenuineBat <genuine_bat@Greg.>
2025-05-10 21:53:55 +0800

    reworked weights and biases management. weights calculation needs to be reworked fully.

--- nn.py
+++ nn.py
@@ -6,7 +6,7 @@ LAYERS = [1, 3, 4, 1]
 EPOCH = 1
 
 # create file, if doesn't exist, to store weights and biases
-if not os.path.isfile("weights_biases.txt"):
+if not os.path.isfile("weights.txt") and not os.path.isfile("biases.txt"):
 	weights = [[str(round(r.uniform(-0.5, 0.5), 2)) for _ in range(LAYERS[n]*LAYERS[n+1])] for n in range(len(LAYERS)-1)]
 	biases = [["0" for _ in range(n)] for n in LAYERS[1:-1]]
 
@@ -15,43 +15,40 @@ if not os.path.isfile("weights_biases.txt"):
 	print(f"Biases: {biases}")
 
 	with open("weights.txt", "w") as f:
-		print("before prep")
-		print(weights)
 		weights = [",".join(i) for i in weights]
 		weights = "\n".join(weights)
-		print("after prep")
-		print(weights)
 		f.write(weights)
 	
 	with open("biases.txt", "w") as f:
+		biases = [",".join(i) for i in biases]
 		biases = "\n".join(biases)
+		print("biases:")
+		print(biases)
 		f.write(biases)
 
 def get_weights_biases():
 	w = [i.split(",") for i in open("weights.txt", "r").read().split("\n")]
-	b = open("weights.txt", "r").read().split("\n")
+	b = [i.split(",") for i in open("biases.txt", "r").read().split("\n")]
 
+	print("weights")
+	print(w)
+	print("biases")
+	print(b)
 	w = [[float(j) for j in i] for i in w]
 	b = [[float(j) for j in i] for i in b]
 	return w, b
 
 w, b = get_weights_biases()
-print("weights:")
-print(type(w))
-print(w)
-print("biases:")
-print(type(b))
-print(b)
 h_layers = []
 
 for i in range(len(LAYERS)-1):
-	if i == len(LAYERS)-1:
-		h_layers.append(HLayer(LAYERS[i], LAYERS[i+1], w[i], null))
+	if i == len(LAYERS)-2:
+		h_layers.append(HLayer(LAYERS[i], LAYERS[i+1], [w[i]], None))
 	else:
-		h_layers.append(HLayer(LAYERS[i], LAYERS[i+1], w[i], b[i]))	
+		h_layers.append(HLayer(LAYERS[i], LAYERS[i+1], [w[i]], [b[i]]))	
 
 for e in range(EPOCH):
-	curr_l_output = [2] #temp input value
+	curr_l_output = [[2]] #temp input value
 	for l in h_layers:
 		l.forward(curr_l_output)
 
