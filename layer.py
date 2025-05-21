b138d5a0b1d3876ed958f297c16efb6ba7bfcec6
GenuineBat <genuine_bat@Greg.>
2025-05-21 12:29:52 +0800

    finished biases calculation

--- layer.py
+++ layer.py
@@ -8,6 +8,8 @@ class HLayer():
 		self.biases = _biases
 
 	def add_matrix(self, a, b):
+		if not b:
+			return a
 		return [[a[i][j] + b[i][j] for j in range(len(a[0]))] for i in range(len(a))]
 
 	def multiply_matrix(self, a, b):
@@ -28,10 +30,16 @@ class HLayer():
 		print(self.weights)
 		print("after multiplication:")
 		print(self.multiply_matrix(inputs, self.weights))
-		#print("biases")
-		#print(self.biases)
-		#print("final res")
-		#print(self.add_matrix(self.multiply_matrix(inputs, self.weights), self.biases))
+		print("biases")
+		print(self.biases)
+		print("final res")
+		print(self.add_matrix(self.multiply_matrix(inputs, self.weights), self.biases))
 
-		#return self.add_matrix(self.multiply_matrix(inputs, self.weights), self.biases)
-		return self.multiply_matrix(inputs, self.weights)
+		res = self.multiply_matrix(inputs, self.weights)
+		res = self.add_matrix(res, self.biases)
+		res = self.activate(res)
+
+		return res
+
+	def activate(self, r):
+		return r
--- nn.py
+++ nn.py
@@ -11,7 +11,7 @@ if not os.path.isfile("weights.txt") and not os.path.isfile("biases.txt"):
 	weights = []
 	for i in range(len(LAYERS)-1):
 		weights.append([[str(round(r.uniform(-0.5, 0.5), 2)) for _ in range(LAYERS[i+1])] for _ in range(LAYERS[i])])
-	biases = [["0" for _ in range(n)] for n in LAYERS[1:-1]]
+	biases = [[["0" for _ in range(n)]] for n in LAYERS[1:-1]]
 
 	print("Weights and Biases generated and saved in weights.txt and biases.txt respectively")
 	print(f"Weights: {weights}")
@@ -21,14 +21,14 @@ if not os.path.isfile("weights.txt") and not os.path.isfile("biases.txt"):
 		f.write(str(weights))
 	
 	with open("biases.txt", "w") as f:
-		biases = [",".join(i) for i in biases]
-		biases = "\n".join(biases)
-		f.write(biases)
+		f.write(str(biases))
 
 def get_weights_biases():
 	w = ast.literal_eval(open("weights.txt", "r").read())
 	w = [[[float(k) for k in j] for j in i] for i in w]
-	b = [i.split(",") for i in open("biases.txt", "r").read().split("\n")]
+
+	b = ast.literal_eval(open("biases.txt", "r").read())
+	b = [[[float(k) for k in j] for j in i] for i in b]
 
 	return w, b
 
@@ -39,7 +39,7 @@ for i in range(len(LAYERS)-1):
 	if i == len(LAYERS)-2:
 		h_layers.append(HLayer(LAYERS[i], LAYERS[i+1], w[i], None))
 	else:
-		h_layers.append(HLayer(LAYERS[i], LAYERS[i+1], w[i], [b[i]]))	
+		h_layers.append(HLayer(LAYERS[i], LAYERS[i+1], w[i], b[i]))	
 
 for e in range(EPOCH):
 	curr_l_output = [[2]] #temp input value
