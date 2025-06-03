2d1fdc0d2e7777e27e1f037f85772393d96f5368
GenuineBat <genuine_bat@Greg.>
2025-06-03 18:11:53 +0800

    training structure almost done, still figuring out calculations

--- model.py
+++ model.py
@@ -4,9 +4,8 @@ import ast
 import random as r
 
 class NeuralNetworkModel():
-	def __init__(self, _layout, _batch_size):
+	def __init__(self, _layout):
 		self.layout = _layout 
-		self.batch_size = _batch_size 
 		self.h_layers = []
 
 		self.set_weights()
@@ -19,7 +18,7 @@ class NeuralNetworkModel():
 			for i in range(len(self.layout)-1):
 				weights.append([[str(round(r.uniform(-0.5, 0.5), 5)) for _ in range(self.layout[i+1])] for _ in range(self.layout[i])])
 
-			biases = [[["0" for _ in range(n)]] for n in self.layout[1:-1]]
+			biases = [[["0.01" for _ in range(n)]] for n in self.layout[1:-1]]
 
 			print("New Weights and Biases generated and saved in model_parameters folder")
 
@@ -42,7 +41,7 @@ class NeuralNetworkModel():
 		w, b = self.get_weights_biases()
 
 		print(f"Weights retrieved from model_parameters: {w}")
-		print(f"Biases retrieved from model_parameters: {b}")
+		print(f"Biases retrieved from model_parameters: {b}\n")
 
 		for i in range(len(self.layout)-1):
 			if i == len(self.layout)-2:
@@ -53,22 +52,23 @@ class NeuralNetworkModel():
 	def train(self, epoch, bs, lr, data):
 		for e in range(epoch):
 			for i in range(0, len(data), bs):
-				batch_outputs = [[] for _ in bs]
+				batch_outputs = [[] for _ in range(bs)]
 				for b in range(bs):
-					curr_l_output = data[i+b]
+					curr_l_output = [[data[i+b][0]]]
 					for l in self.h_layers:	
 						curr_l_output = l.forward(curr_l_output)
-						outputs_by_batch[b].append(curr_l_output)
+						batch_outputs[b].append(curr_l_output)
 				self.modify_weights(batch_outputs)
 
 	def predict(self, data):
-		res = [{"predicted value": 0, "expected value": 0, "cost": 0} for _ in data]
+		res = [{"input value": 0, "predicted value": 0, "expected value": 0, "cost": 0} for _ in data]
 
 		for d in range(len(data)):
 			curr_l_output = [[data[d][0]]]
 			for l in self.h_layers:
 				curr_l_output = l.forward(curr_l_output)
 				
+			res[d]["input value"] = data[d][0]
 			res[d]["predicted value"] = curr_l_output[0][0]
 			res[d]["expected value"] = data[d][1]
 			res[d]["cost"] = abs(res[d]["predicted value"] - res[d]["expected value"])
@@ -76,7 +76,8 @@ class NeuralNetworkModel():
 		return res
 
 	def modify_weights(self, batch_outputs):
-		pass
+		print("batch outputs:")
+		print(batch_outputs)	
 
 	def save_weights(self):
 		pass
@@ -98,31 +99,33 @@ class NeuralNetworkModel():
 				for j in range(len(b[0])):
 					for k in range(len(b)):
 						res[i][j] = res[i][j] + (a[i][k] * b[k][j])
-					res[i][j] = round(res[i][j], 2)
+					res[i][j] = res[i][j]
 
 			return res
 		
 		def forward(self, inputs):
-			print("\n")
-			print("input:")
+			print("inputs")
 			print(inputs)
-			print("weights:")
+			print("weights")
 			print(self.weights)
-			res = self.multiply_matrix(inputs, self.weights)
-			print("after mulitplication:")
-			print(res)
-			
-			print("biases:")
+			print("biases")
 			print(self.biases)
-			res = self.add_matrix(res, self.biases)
-			print("after adding biases")
+			res = self.multiply_matrix(inputs, self.weights)
+			print("after mult")
 			print(res)
-
+			if self.biases != None:
+				res = self.add_matrix(res, self.biases)
+			print("before activ")
+			print(res)
 			res = self.relu(res)
-			print("after activation")
+			print("after activ")
 			print(res)
+			print("\n")
 
 			return res
+
+		def sigmoid(self, r):
+			return [[round(1/(1+math.exp(-j)), 6) for j in i] for i in r]
 
 		def relu(self, r):
 			return r if r[0][0] > 0 else [[0 for _ in r[0]]]
--- train.py
+++ train.py
@@ -1,16 +1,25 @@
 from model import NeuralNetworkModel 
+import random as r
 
-def get_training_data():
-	return [[24, 1], [65, 0]] #is this an even number? yes (1) no (0)
+def get_training_data(n):
+	data = []
+	for _ in range(n):
+		d = [float(r.randint(0,99))]
+		d.append(d[0]%2)
+		data.append(d)
+	
+	return data
 
 LAYOUT = [1, 3, 4, 1]
-EPOCH = 1
+EPOCH = int(1)
 LEARNING_RATE = 0.01
-BATCH_SIZE = 10
+BATCH_SIZE = 3
 
 nnm = NeuralNetworkModel(LAYOUT)
-training_data = get_training_data()
+training_data = get_training_data(6)
 
-res = nnm.predict(training_data)
+#res = nnm.predict(training_data)
 
-print(f"\nModel prediction value(s):\n{res}")
+#print(f"\nModel prediction value(s):\n{res}")
+
+nnm.train(EPOCH, BATCH_SIZE, LEARNING_RATE, training_data)
